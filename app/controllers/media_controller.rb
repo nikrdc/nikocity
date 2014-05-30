be32c716c6117ce642cd26faae02764e8b500e3c
nikrdc <nikrdc@gmail.com>
2014-05-30 14:42:15 -0400

    implemented image uploading with paperclip and created _show partial

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -76,6 +76,6 @@ class MediaController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def medium_params
-      params.require(:medium).permit(:name, :meta, :kind)
+      params.require(:medium).permit(:name, :meta, :kind, :image)
     end
 end
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,6 @@
 class Medium < ActiveRecord::Base
-  validates :name, :meta, :kind, presence: true
-  validates :kind, inclusion: { in: ['Movie', 'TV show', 'Video game', 'Music', 'Book'] }  
+  validates :name, :meta, :kind, :image, presence: true
+  validates :kind, inclusion: { in: ['Movie', 'TV show', 'Video game', 'Music', 'Book'] }
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
